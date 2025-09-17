9864e80ecd39fa2c2be7763ddb1d8906e15a8740
Mikhail Boin <misoin2807@gmail.com>
2025-09-17 16:12:06 +0300

    feat: removed unused method

--- src/main/java/org/example/transactionservice/repository/specs/TransactionSpecs.java
+++ src/main/java/org/example/transactionservice/repository/specs/TransactionSpecs.java
@@ -3,38 +3,32 @@ package org.example.transactionservice.repository.specs;
 import org.example.transactionservice.model.entity.Transaction;
 import org.springframework.data.jpa.domain.Specification;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
+import java.util.Objects;
 
 public class TransactionSpecs {
 
-    final static LocalDateTime minDate = LocalDateTime.of(LocalDate.of(1000, 10, 10), LocalTime.of(10, 10));
-
-    public static Specification<Transaction> dateBetween(LocalDateTime startDate, LocalDateTime endDate) {
-        if (startDate == null) {
-            startDate = minDate;
-        }
-        if (endDate == null) {
-            endDate = LocalDateTime.now();
-        }
-
-        final var finalStartDate = startDate;
-        final var finalEndDate = endDate;
-        return (root, query, builder) ->
-                builder.between(root.get("dateTime"), finalStartDate, finalEndDate);
-    }
-
     public static Specification<Transaction> dateAfter(LocalDateTime date) {
-        return dateBetween(date, null);
+        Objects.requireNonNull(date, "date must not be null");
+        return (root, query, builder) ->
+                builder.greaterThanOrEqualTo(root.get("dateTime"), date);
     }
 
     public static Specification<Transaction> dateBefore(LocalDateTime date) {
-        return dateBetween(null, date);
+        Objects.requireNonNull(date, "date must not be null");
+        return (root, query, builder) ->
+                builder.lessThanOrEqualTo(root.get("dateTime"), date);
     }
 
     public static Specification<Transaction> byCustomerId(String customerId) {
+        Objects.requireNonNull(customerId, "customerId must not be null");
         return (root, query,  builder) ->
                 builder.equal(root.get("customerId"), customerId);
+    }
+
+    public static Specification<Transaction> byAccountId(String accountId) {
+        Objects.requireNonNull(accountId, "customerName must not be null");
+        return (root, query,  builder) ->
+                builder.equal(root.get("accountId"), accountId);
     }
 }
